ad8c2e3d0c342be9eebe15661170084173b735ae
JagMarques1404 <joao.melanciatv@gmail.com>
2025-10-02 14:06:49 -0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,8 +1,4 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json')
-  res.status(200).end(JSON.stringify({
-    ok: true,
-    timestamp: Date.now(),
-    message: 'API funcionando!'
-  }))
+module.exports = (req, res) => {
+  res.setHeader('Content-Type','application/json')
+  res.status(200).end(JSON.stringify({ ok: true, ts: Date.now() }))
 }
