f69ef2bece9f0b1acc91605719e5008fee9e1b35
JagMarques1404 <joao.melanciatv@gmail.com>
2025-10-02 11:30:39 -0300

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).end(JSON.stringify({
+    ok: true,
+    timestamp: Date.now(),
+    message: 'API funcionando!'
+  }))
+}
